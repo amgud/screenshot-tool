6e8604f5672aa1862e6dbe41550a15bdc84d89cf
Maksim Gudkov <maksim.gudkov@spark.co.nz>
2025-05-01 23:16:18 +1200

    refactor: remove inline styles from popup and side panel, enhance CSS for better UI consistency

--- popup.js
+++
@@ -1,7 +0,0 @@
-document
-  .getElementById('toggleSidePanel')
-  .addEventListener('click', async () => {
-    const { id } = await chrome.windows.getCurrent();
-    chrome.sidePanel.open({ windowId: id });
-    console.log('Side panel opened');
-  });
